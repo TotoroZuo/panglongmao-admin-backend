236461364b6378610c81a454049fbe8cc4e9f723
longmaomao <zlf@zuolongfei.me>
2018-10-30 00:53:18 +0800

    提交代码

---
+++ app.js
@@ -0,0 +1,59 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// 配置信息
+const config = require('./config/config')
+// 请求拦截
+const interceptors = require('./middlreware/interceptors')
+// 过滤不用jwt验证
+app.use(jwt({
+  secret: config.SECRET
+}).unless({
+  path: [
+    // 登录接口
+    /^\/login/,
+  ]
+}))
+
+app.use(interceptors())
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
---
+++ config/config.js
@@ -0,0 +1,12 @@
+// 加密密匙
+const SRCRET = 'loangmao';
+// mysql 帐号
+const MYSQL_ACCOUNT ='root';
+// mysql 密码
+const MYSQL_PASSWORD="longmaomao";
+
+module.exports={
+    SRCRET,
+    MYSQL_ACCOUNT,
+    MYSQL_PASSWORD
+}
